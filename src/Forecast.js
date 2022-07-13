3a2a978e4ab3e6bf53f3cce7f308d9d8322e1291
bsarnii <scharnoui@gmail.com>
2022-07-13 14:07:21 +0200

    updated date

--- src/Forecast.js
+++ src/Forecast.js
@@ -3,24 +3,27 @@ import './Forecast.css';
 import ForecastDay from './ForecastDay';
 
 function Forecast({results}) {
+ let date1=results.forecast.forecastday[0].date.split("-").reverse().join(".");
+ let date2=results.forecast.forecastday[1].date.split("-").reverse().join(".");
+ let date3=results.forecast.forecastday[2].date.split("-").reverse().join(".");
   return (
     <div className='forecast'>
        <ForecastDay 
-       date={results.forecast.forecastday[0].date}
+       date={date1}
        imgSrc={results.forecast.forecastday[0].day.condition.icon}
        imgAlt={results.forecast.forecastday[0].day.condition.text} 
        maxtemp={results.forecast.forecastday[0].day.maxtemp_c + "°"} 
        mintemp={results.forecast.forecastday[0].day.mintemp_c  + "°" } 
        text={results.forecast.forecastday[0].day.condition.text} />
        <ForecastDay 
-       date={results.forecast.forecastday[1].date}
+       date={date2}
        imgSrc={results.forecast.forecastday[1].day.condition.icon}
        imgAlt={results.forecast.forecastday[1].day.condition.text} 
        maxtemp={results.forecast.forecastday[1].day.maxtemp_c + "°"} 
        mintemp={results.forecast.forecastday[1].day.mintemp_c  + "°" } 
        text={results.forecast.forecastday[1].day.condition.text} />
        <ForecastDay 
-       date={results.forecast.forecastday[2].date}
+       date={date3}
        imgSrc={results.forecast.forecastday[2].day.condition.icon}
        imgAlt={results.forecast.forecastday[2].day.condition.text} 
        maxtemp={results.forecast.forecastday[2].day.maxtemp_c + "°"} 
